ea9d4caa0df70ff2758de6562236b721269d8195
Louis Duboz <105061897+Louis-Duboz@users.noreply.github.com>
2024-10-08 17:20:43 +0200

    init: added SDL3 + configured cmake project

---
+++ client/include/main.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+#include <SDL3/SDL.h>
+#include <iostream>
---
+++ client/src/main.cpp
@@ -0,0 +1,97 @@
+﻿#include "main.h"
+/*
+void createWindow(const char* title, int width, int height, Uint32 flags) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+    if (!window) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window: %s", SDL_GetError());
+        SDL_Quit();
+        return;
+    }
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL, 0);
+    if (!renderer) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return;
+    }
+
+    // Set window background color to black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+
+    // Main loop to keep the window open
+    bool isRunning = true;
+    SDL_Event event;
+    while (isRunning) {
+        // Poll for events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
+
+        // Clear the screen with the background color
+        SDL_RenderClear(renderer);
+
+        // Present the renderer (update the window)
+        SDL_RenderPresent(renderer);
+    }
+
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    // Parameters for the window
+    const char* title = "My SDL3 Window";
+    int width = 800;
+    int height = 600;
+    Uint32 flags = SDL_WINDOW_RESIZABLE;  // You can change this to SDL_WINDOW_FULLSCREEN, etc.
+
+    // Create the window with the specified parameters
+    createWindow(title, width, height, flags);
+
+    return 0;
+}*/
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window* window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "Gravity Simulation",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(30000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+    return 0;
+}
