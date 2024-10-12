cfd0db19d05d55e4454e3a48c00337a971d35312
Louis Duboz <105061897+Louis-Duboz@users.noreply.github.com>
2024-10-12 16:08:16 +0200

    feat: basic loop to test the gravitation in physics-engine.

--- source/console/source/main.cpp
+++ source/console/source/main.cpp
@@ -2,50 +2,68 @@
 #include "framework.h"
 #include "main.h"
 
-// Component types
-struct Position {
-    double x;
-    double y;
-};
+#include <thread>
 
-struct Velocity {
-    double x;
-    double y;
-};
+#include "Simulation.hpp"
+#include "Components.hpp"
 
-// Tag types
-struct Eats { };
-struct Apples { };
+#ifdef _WIN32
+	#define CLEAR_SCREEN "cls"
+#else
+	#define CLEAR_SCREEN "clear"
+#endif
 
 int main(int, char* []) {
-    // Create the world
     flecs::world ecs;
+    Simulation::initialise(ecs);
 
-    // Register system
-    ecs.system<Position, Velocity>()
-        .each([](Position& p, Velocity& v) {
-        p.x += v.x;
-        p.y += v.y;
-            });
-
-    // Create an entity with name Bob, add Position and food preference
-    flecs::entity Bob = ecs.entity("Bob")
+    /*
+    flecs::entity sun = ecs.entity("Sun")
         .set(Position{ 0, 0 })
-        .set(Velocity{ 1, 2 })
-        .add<Eats, Apples>();
+        .set(Velocity{ 0, 0 })
+        .set(Acceleration{ 0, 0 })
+        .set(Mass{ 1.989e30 });
 
-    // Show us what you got
-    std::cout << Bob.name() << "'s got [" << Bob.type().str() << "]\n";
+    flecs::entity planet = ecs.entity("Earth")
+        .set(Position{ 149597870000, 0 })
+        .set(Velocity{ 0, 29800 })
+        .set(Acceleration{ 0, 0 })
+        .set(Mass{ 5.972e24 });
+    */
 
-    // Run systems twice. Usually this function is called once per frame
-    ecs.progress();
-    ecs.progress();
+    flecs::entity A = ecs.entity("A")
+        .set(Position{ -10, 0 })
+        .set(Velocity{ 0, -2 })
+        .set(Acceleration{ 0, 0 })
+        .set(Mass{ 100 });
 
-    // See if Bob has moved (he has)
-    const Position* p = Bob.get<Position>();
-    std::cout << Bob.name() << "'s position is {" << p->x << ", " << p->y << "}\n";
+    flecs::entity B = ecs.entity("B")
+        .set(Position{ 10, 0 })
+        .set(Velocity{ 0, 2 })
+        .set(Acceleration{ 0, 0 })
+        .set(Mass{ 100 });
 
-    // Output
-    //  Bob's got [Position, Velocity, (Identifier,Name), (Eats,Apples)]
-    //  Bob's position is {2, 4}
+    bool isRunning = true;
+    constexpr int TICK_MAXIMUM = 2000;
+    int i = 0;
+
+    while (isRunning)
+    {
+        std::system(CLEAR_SCREEN);
+        std::cout << "\nIteration : " << i++ << "\n";
+        Simulation::update(ecs, 0.04f);
+
+    	ecs.each([&](const flecs::entity entity, const Position& pos, const Velocity& vel, const Acceleration& acc, const Mass& mass) {
+            std::cout << entity.name()
+                << ": [pos: { " << pos.x << ", " << pos.y << " }"
+                <<   " vel: { " << vel.x << ", " << vel.y << " }"
+                <<   " acc: { " << acc.x << ", " << acc.y << " }"
+                <<   " mass: { " << mass.value << " }]\n";
+        });
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+        if (i >= TICK_MAXIMUM)
+            isRunning = false;
+    }
 }
